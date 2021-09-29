2be168c680e5d45334a8eeeca7ea456c7392bb71
Christopher Bartling <chris.bartling@gmail.com>
2021-09-28 23:15:56 -0500

    Investigating NumPy functions

---
+++ NumPyIntroduction/numpy_lists.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+mylist = [1, 2, 3, 4]
+
+print(type(mylist))
+
+np_array = np.array(mylist)
+
+print(type(np_array))
+
+my_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print(type(my_matrix))
+
+np_matrix = np.array(my_matrix)
+
+print(type(np_matrix))
+
+print(np.arange(0, 10))
+
+print(np.zeros(5))
+
+
+
+
+
